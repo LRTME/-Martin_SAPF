68a66e6282007a02a09b196a29fc2c957b6a45d2
MartinRozej <martin.rozej@gmail.com>
2017-02-22 12:29:31 +0100

    Dodal funkcije WD kick, relejev, LED, stikal

--- include/PCB_util.h
+++ include/PCB_util.h
@@ -11,14 +11,107 @@
 #include    "define.h"
 
 /**************************************************************
+* Funckija ki brcne zunanji WatchDog (WD_KICK) se lahko
+* kliče samo iz periodicne prekinitve
+**************************************************************/
+extern void PCB_WD_KICK_int(void);
+
+
+/**************************************************************
+* Funkcije za vklop/izklop relejev
+***************************************************************
+* Funckija, ki vklopi Rele1 (Supply_main_relay)
+**************************************************************/
+extern void PCB_relay1_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi Rele1 (Supply_main_relay)
+**************************************************************/
+extern void PCB_relay1_off(void);
+
+/**************************************************************
+* Funckija, ki vklopi Rele2 (Supply_resistor_relay)
+**************************************************************/
+extern void PCB_relay2_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi Rele2 (Supply_resistor_relay)
+**************************************************************/
+extern void PCB_relay2_off(void);
+
+/**************************************************************
+* Funckija, ki vklopi Rele3 (Filter_main_relay)
+**************************************************************/
+extern void PCB_relay3_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi Rele3 (Filter_main_relay)
+**************************************************************/
+extern void PCB_relay3_off(void);
+
+
+/**************************************************************
+* Funkcije za vklop/izklop LED
+***************************************************************
+* Funckija, ki vklopi LED FAULT (SIG_FAULT)
+**************************************************************/
+extern void PCB_LED_FAULT_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi LED FAULT (SIG_FAULT)
+**************************************************************/
+extern void PCB_LED_FAULT_off(void);
+
+/**************************************************************
+* Funckija, ki vklopi LED READY (SIG_READY)
+**************************************************************/
+extern void PCB_LED_READY_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi LED READY (SIG_READY)
+**************************************************************/
+extern void PCB_LED_READY_off(void);
+
+/**************************************************************
+* Funckija, ki vklopi LED WORKING (SIG_WORKING)
+**************************************************************/
+extern void PCB_LED_WORKING_on(void);
+
+/**************************************************************
+* Funckija, ki izklopi LED WORKING (SIG_WORKING)
+**************************************************************/
+extern void PCB_LED_WORKING_off(void);
+
+
+/**************************************************************
+* Funkcije za stanje tipk
+***************************************************************
+* Funckija ki vrne stanje ENABLE tipke (SW_ENABLE)
+**************************************************************/
+extern bool PCB_SW_ENABLE(void);
+
+/**************************************************************
+* Funckija ki vrne stanje RESET tipke (SW_RESET)
+**************************************************************/
+extern bool PCB_SW_RESET(void);
+
+
+/**************************************************************
 * Funkcije ki manipulirajo LED diodo  na ControlCardu
 **************************************************************/
 extern void PCB_LEDcard_on(void);
 extern void PCB_LEDcard_off(void);
 extern void PCB_LEDcard_toggle(void);
 
+
 /**************************************************************
-* Funkcija ki inicializa MCU in tiskanino
+* Funckija ki vrne stanje strojne zascite
+**************************************************************/
+extern bool PCB_HW_trip(void);
+
+
+/**************************************************************
+* Funckija ki inicializira tiskanino
 **************************************************************/
 extern void PCB_init(void);
 
