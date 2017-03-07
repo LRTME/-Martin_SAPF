c07dfc6040682ab9ac2586d2f3cbe0d9d2b71b9a
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2017-03-07 10:43:42 +0100

    Dodal cel CPLD projekt

---
+++ CPLD/_impact.cmd
@@ -0,0 +1,128 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+setPreference -pref StartupClock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref svfUseTime:FALSE
+setPreference -pref SpiByteSwap:AUTO_CORRECTION
+setPreference -pref AutoInfer:TRUE
+setPreference -pref SvfPlayDisplayComments:FALSE
+setPreference -pref StartupClock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref svfUseTime:FALSE
+setPreference -pref SpiByteSwap:AUTO_CORRECTION
+setPreference -pref AutoInfer:TRUE
+setPreference -pref SvfPlayDisplayComments:FALSE
+setPreference -pref StartupClock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref ConfigOnFailure:STOP
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref svfUseTime:FALSE
+setPreference -pref SpiByteSwap:AUTO_CORRECTION
+setPreference -pref AutoInfer:TRUE
+setPreference -pref SvfPlayDisplayComments:FALSE
+setPreference -pref StartupClock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref ConfigOnFailure:STOP
+setPreference -pref UserLevel:NOVICE
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref svfUseTime:FALSE
+setPreference -pref SpiByteSwap:AUTO_CORRECTION
+setPreference -pref AutoInfer:TRUE
+setPreference -pref SvfPlayDisplayComments:FALSE
+setPreference -pref StartupClock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref ConfigOnFailure:STOP
+setPreference -pref UserLevel:NOVICE
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref svfUseTime:FALSE
+setPreference -pref SpiByteSwap:AUTO_CORRECTION
+setPreference -pref AutoInfer:TRUE
+setPreference -pref SvfPlayDisplayComments:FALSE
+setCable -port auto
+addDevice -p 1 -file "D:/Mitja/Projekti/SerijskiFilterEnoF/Programi/CPLD_logika/Logika.jed"
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:\Mitja\Projekti\SerijskiFilterEnoF\Programi\CPLD_logika\\auto_project.ipf"
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:\Mitja\Projekti\SerijskiFilterEnoF\Programi\CPLD_logika\\auto_project.ipf"
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:\Mitja\Projekti\SerijskiFilterEnoF\Programi\CPLD_logika\\auto_project.ipf"
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:\Mitja\Projekti\SerijskiFilterEnoF\Programi\CPLD_logika\\auto_project.ipf"
+setCable -port auto
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "D:\Mitja\Projekti\SerijskiFilterEnoF\Programi\CPLD_logika\\auto_project.ipf"
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
